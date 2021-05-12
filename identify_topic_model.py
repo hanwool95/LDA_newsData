201f004c397c47fcf7b1796710595cdf12e82adf
hanwool <hanwool95@snu.ac.kr>
2021-05-13 02:49:25 +0900

    revising exception list

--- identify_topic_model.py
+++ identify_topic_model.py
@@ -1,10 +1,15 @@
 from gensim.models import LdaModel
 from gensim.test.utils import datapath
+import csv
 
 temp_file = datapath("full_content_only")
 lda = LdaModel.load(temp_file)
 
 
 topics = lda.print_topics(num_words=20)
+
+f = open("LDA_result.csv", 'w', newline="")
+wr = csv.writer(f)
 for topic in topics:
-    print(topic)
+    wr.writerow(topic)
+f.close()
